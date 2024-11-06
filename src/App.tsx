e7488bc59482e0128c7613b05fac1335fea80ae8
wellitsabhi <abhishekongit@gmail.com>
2024-11-06 12:56:06 +0530

    variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { motion } from "framer-motion";
-import BouncingLoader from "./components/BouncingLoader";
+// import BouncingLoader from "./components/BouncingLoader";
+import { useState } from "react";
+import {variants } from "./variants.ts";
 const App = () => {
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <div>
-      <BouncingLoader />
+
+        <motion.div className="box" 
+          variants={variants}
+          initial ="hidden"
+          animate={isVisible ? "visible" : "hidden"}
+          exit ="exit"
+          transition={{ duration:2}}
+          onMouseEnter={() => setIsVisible(!isVisible)}
+          onMouseLeave={() => setIsVisible(!isVisible)}
+        ></motion.div>
+
+
+      {/* <BouncingLoader /> */}
     </div>
   );
 };
---
+++ src/variants.ts
@@ -0,0 +1,5 @@
+export const variants = {
+    hidden: {opacity: 0.1 , scale: 0.7},
+    visible: {opacity: 1, scale: 1,},
+    exit: {opacity: 0.1, scale: 0.5}
+  }
