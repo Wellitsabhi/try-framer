a93b1ece084ef1265618a919a7a62e91bb7f2ebd
wellitsabhi <abhishekongit@gmail.com>
2024-11-06 13:50:23 +0530

    FlippingCard, minor issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import { motion } from "framer-motion";
-// import BouncingLoader from "./components/BouncingLoader";
-import { useState } from "react";
-import {variants } from "./variants.ts";
+import FlippingCard from "./components/FlippingCard.tsx";
 const App = () => {
-  const [isVisible, setIsVisible] = useState(false);
   return (
     <div>
-
-        <motion.div className="box" 
-          variants={variants}
-          initial ="hidden"
-          animate={isVisible ? "visible" : "hidden"}
-          exit ="exit"
-          transition={{ duration:2}}
-          onMouseEnter={() => setIsVisible(!isVisible)}
-          onMouseLeave={() => setIsVisible(!isVisible)}
-        ></motion.div>
-
-
-      {/* <BouncingLoader /> */}
+      <FlippingCard />
     </div>
   );
 };
