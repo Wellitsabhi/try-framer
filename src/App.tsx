12cad4e2bd03d91ed866a4a7e58cb9c86e9abfd3
wellitsabhi <abhishekongit@gmail.com>
2024-11-06 15:01:46 +0530

    Animated Csard

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
+import AnimatedCard from "./components/AnimatedCard";
 const App = () => {
-  return <div>
-    <motion.div 
-    className="box " 
-    drag 
-    dragConstraints = {{top: -50, left: -50, bottom: 50, right: 50}}
-    whileDrag={{scale: 0.7,
-      // backgroundColor: "whitesmoke"
-    }}
-    transition={{ type: "spring", stiffness: 100}}
-
-    />
-
+  return (
+    <div>
+      <AnimatedCard />
     </div>
-  
+  );
 };
 
 export default App;
