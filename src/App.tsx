29deff54cf647ac7683370288f6d91d6fa875827
wellitsabhi <abhishekongit@gmail.com>
2024-11-07 11:10:57 +0530

    useMotionValue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { motion } from "framer-motion";
+import { motion, useMotionValue, useMotionValueEvent } from "framer-motion";
 import StaggerAnimation from "./components/StaggerAnimation";
 const App = () => {
+  const x =useMotionValue(200);
+  useMotionValueEvent(x, 'animationStart', ()=>{
+    console.log('animationStarted on x');
+  })
+
+  useMotionValueEvent(x, 'change', (latest)=>{
+    console.log('x change to ', latest);
+  })
+
+
   return (
-    <div>
-      <StaggerAnimation/>
-    </div>
+    <motion.div className="box" drag dragConstraints={{left: 0, right: 200, }} style={{x}}>
+    </motion.div>
   );
 };
 
